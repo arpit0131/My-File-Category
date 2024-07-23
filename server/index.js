69d4efd0475beb6a9b956bd571f30b17fe3c7d38
Arpit Gautam <arpitgautam73@gmail.com>
2024-07-24 00:47:29 +0530

    added

--- server/index.js
+++ server/index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { scanDirectory } = require('./file-scan.js');
+const path = require('path');
 
-app.use(cors());
+const clientBuildPath = path.join(__dirname, '../client/build');
+console.log('Client build path:-', clientBuildPath);
+app.use(express.static(clientBuildPath));
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
 app.post('/scan', async (req, res) => {
@@ -21,6 +30,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('*', (req, res) => { 
+  res.sendFile(path.join(clientBuildPath, 'index.html'));
+})
 const PORT = 9090;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
